f645ddfdb69353a64ca969f716d6db651ff6d657
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-01-28 14:35:46 +0900

    feat: Read a File and Find K

---
+++ find_K.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define SIZE 500000
+int main(int arge, char *argv[])
+{
+    int n;
+    int sum_without_K = 0;
+    scanf("%d", &n);
+    // 1부터 N의 숫자중 K가 빠진 배열
+    int partArr[SIZE];
+    int lengthOfPartArr = n - 1;
+    for (int i = 0; i < lengthOfPartArr; i++)
+    {
+        scanf("%d", &partArr[i]);
+    }
+    // TODO: n과 partArr를 이용하여, K를 구하라
+
+    // 1부터 N까지의 합
+    int sum_including_K = n * (n + 1) * 0.5;
+
+    // 배열의 합
+    for (int i = 0; i < lengthOfPartArr; i++)
+    {
+        sum_without_K += partArr[i];
+    }
+
+    // K = 1부터 N까지의 합 - 파일 내 숫자의 합
+    int K = sum_including_K - sum_without_K;
+    printf("K = %i", K);
+    return 0;
+}
